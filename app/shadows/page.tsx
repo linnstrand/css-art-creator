be1a7be34f61a8b767c510a54174c20cdeec76cc
Your Name <you@example.com>
2023-09-24 21:35:11 +0200

    dynamicing

--- app/shadows/page.tsx
+++ app/shadows/page.tsx
@@ -17,15 +17,62 @@ type Props = {
 
 const ShapeArea = ({ corners, size }: Props) => {
   return (
-    <div
-      className="shadow-shape"
-      style={{ height: `${size}em`, width: `${size}em`, fontSize: "90%" }}
-    >
-      <div className="shard segment-1" style={{}}></div>
-      <div className="shard segment-2"></div>
-      <div className="shard segment-3"></div>
-      <ShapeSegment />
-    </div>
+    <>
+      <div
+        className="shadow-shape"
+        style={{ height: `${size}em`, width: `${size}em`, fontSize: "90%" }}
+      >
+        <div className="shard segment-1" style={{}}></div>
+        <div className="shard segment-2"></div>
+        <div className="shard segment-3"></div>
+      </div>
+      <div
+        className="shadow-shape"
+        style={{
+          height: `${size}em`,
+          width: `${size}em`,
+          fontSize: "90%",
+          top: "10rem",
+        }}
+      >
+        <ShapeSegment
+          height={3}
+          width={5}
+          shadows={2}
+          color={"rgb(121, 255, 131)"}
+        />
+        <ShapeSegment
+          height={3}
+          width={5}
+          shadows={2}
+          color={"rgb(121, 255, 131)"}
+        />
+        <ShapeSegment
+          height={3.3}
+          width={5}
+          shadows={2}
+          color={"rgb(121, 255, 131)"}
+        />
+        <ShapeSegment
+          height={3}
+          width={5}
+          shadows={2}
+          color={"rgb(121, 255, 131)"}
+        />
+        <ShapeSegment
+          height={3}
+          width={5}
+          shadows={2}
+          color={"rgb(121, 255, 131)"}
+        />
+        <ShapeSegment
+          height={3}
+          width={10}
+          shadows={1}
+          color={"rgb(121, 255, 131)"}
+        />
+      </div>
+    </>
   );
 };
 
@@ -33,13 +80,17 @@ type Props2 = {
   height: number;
   width: number;
   shadows: number;
+  color: string;
 };
 
 const ShapeSegment = ({ height, width, shadows }: Props2) => {
   const styles = {
     height: height + "em",
-    width: 3 * shadows + "em",
+    width: width * shadows + "em",
+    borderWidth: `0 ${width / 2}em ${height}em ${width / 2}em`,
+    borderBottomColor: "green",
+    filter: `drop-shadow(${width}em 0 rgb(131, 255, 131)) drop-shadow(${width}em 0 rgb(131, 255, 131))`,
   };
 
-  return <div className="shard segment-3" style={styles}></div>;
+  return <div className="shard2" style={styles}></div>;
 };
