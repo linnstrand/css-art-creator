a8deaf53177108e5e09576f328d4cb81dee000aa
Your Name <you@example.com>
2023-09-24 17:56:48 +0200

    hardcoded

--- app/shadows/page.tsx
+++ app/shadows/page.tsx
@@ -19,10 +19,27 @@ const ShapeArea = ({ corners, size }: Props) => {
   return (
     <div
       className="shadow-shape"
-      style={{ height: `${size}em`, width: `${size}em` }}
+      style={{ height: `${size}em`, width: `${size}em`, fontSize: "90%" }}
     >
-      <div className="segment-1" style={{}}></div>
-      <div className="segment-2"></div>
+      <div className="shard segment-1" style={{}}></div>
+      <div className="shard segment-2"></div>
+      <div className="shard segment-3"></div>
+      <ShapeSegment />
     </div>
   );
+};
+
+type Props2 = {
+  height: number;
+  width: number;
+  shadows: number;
+};
+
+const ShapeSegment = ({ height, width, shadows }: Props2) => {
+  const styles = {
+    height: height + "em",
+    width: 3 * shadows + "em",
+  };
+
+  return <div className="shard segment-3" style={styles}></div>;
 };
